5a7e67ec0ca6b2f312aa3303ab538c0b79ea0c9f
fonzerelly <christian.hoerauf@gmail.com>
2021-04-29 01:15:28 +0200

    init

---
+++ precache-manifest.cc996e175fa2ed364218b58d202e56ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c374a65e3b7006f9c09e91fb35906c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f268885aec8ef48892",
+    "url": "/static/css/main.836dbe34.chunk.css"
+  },
+  {
+    "revision": "8b88398bfc670ec31676",
+    "url": "/static/js/2.124ecd39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.124ecd39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f268885aec8ef48892",
+    "url": "/static/js/main.32541662.chunk.js"
+  },
+  {
+    "revision": "ab4f29a9b93c1405ed7d",
+    "url": "/static/js/runtime-main.ef49a28d.js"
+  }
+]);
